29b008d488141499cca65c12b7bceb9d7f4e11aa
Kyle Laracey <kalaracey@gmail.com>
2024-04-05 18:55:26 -0400

    Remove Python 3.10 dependency
    Also add note about `protobuf` and `protoc-wheel-0` compatibility.

--- src/import_proto.py
+++ src/import_proto.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import subprocess
+import sys
 
 import protoc
 
@@ -8,13 +9,19 @@ import protoc
 class ImportProtoException(Exception):
     pass
 
+
 def import_proto(path: str) -> None:
     """Setup python protocol buffer directory."""
 
     _proto_dir = os.path.dirname(os.path.abspath(path))
 
-    _proto_files = glob.glob('**/*.proto', root_dir=_proto_dir, recursive=True)
-    _args = [protoc.PROTOC_EXE, "-I.", "--python_out=.", *_proto_files]
+    if sys.version_info[0] > 3 or (sys.version_info[0] == 3 and sys.version_info[1] >= 10):
+        _proto_files = glob.glob('**/*.proto', root_dir=_proto_dir, recursive=True)
+        _args = [protoc.PROTOC_EXE, "-I.", "--python_out=.", *_proto_files]
+    else:
+        _proto_files = glob.glob(_proto_dir + '/**/*.proto', recursive=True)
+        _proto_files = [os.path.relpath(f, _proto_dir) for f in _proto_files]
+        _args = [protoc.PROTOC_EXE, "-I.", "--python_out=.", *_proto_files]
 
     # print("protoc cwd:", _proto_dir)
     # print("protoc args:", _args)
